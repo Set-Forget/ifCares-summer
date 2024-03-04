9235b21b9b9c3b388db281019ed48e570bfbefa0
hwncito <juanchosperoni@hotmail.com>
2024-03-04 17:55:49 -0300

    First commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="fixed md:relative bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow flex items-center justify-between md:p-6">
+      <span className="text-sm text-gray-500 sm:text-center">
+        Nombre Apellido
+      </span>
+      <span className="flex items-center justify-center text-sm font-medium text-gray-500">
+        <a href="#" className="hover:text-black flex items-center gap-1">
+          Logout
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+            />
+          </svg>
+        </a>
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/siteInfo/SiteInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SiteInfo = () => {
+  return (
+    <div>Im the site information section</div>
+  )
+}
+
+export default SiteInfo
