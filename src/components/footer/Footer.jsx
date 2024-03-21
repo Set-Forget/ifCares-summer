cd0021b55db3f53264b6b3fae3a098af77ec059a
hwncito <juanchosperoni@hotmail.com>
2024-03-21 10:15:36 -0300

    Added Login functionality to the webapp

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,21 @@
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const Footer = () => {
+  const router = useRouter()
+  const logout = () => {
+    // Clear the user cookie
+    document.cookie = 'ifcaresSummer=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    // Redirect to the login page
+    router.push('/auth/login');
+  };
   return (
     <footer className="fixed md:relative bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow flex items-center justify-between md:p-6">
       <span className="text-sm text-gray-500 sm:text-center">
         Name Surname
       </span>
       <span className="flex items-center justify-center text-sm font-medium text-gray-500">
-        <a href="#" className="hover:text-black flex items-center gap-1">
+        <button onClick={logout} className="hover:text-black flex items-center gap-1">
           Logout
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -23,7 +31,7 @@ const Footer = () => {
               d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
             />
           </svg>
-        </a>
+        </button>
       </span>
     </footer>
   );
--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -2,8 +2,23 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
+import { API_URL_ENDPOINT } from '@/constants/constants';
+import { useRouter } from 'next/navigation';
+import Spinner from '../spinner/Spinner';
+
+const setCookie = (name, value, days) => {
+  const expires = new Date(Date.now() + days * 864e5).toUTCString();
+  document.cookie =
+    name +
+    '=' +
+    encodeURIComponent(value) +
+    '; expires=' +
+    expires +
+    '; path=/';
+};
 
 const LoginForm = () => {
+  const router = useRouter();
   const [submitData, setSubmitData] = useState({
     email: '',
     password: '',
@@ -12,6 +27,8 @@ const LoginForm = () => {
     email: '',
     password: '',
   });
+  const [invalidUser, setInvalidUser] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const [emailFocused, setEmailFocused] = useState(false);
   const [passwordFocused, setPasswordFocused] = useState(false);
 
@@ -36,7 +53,33 @@ const LoginForm = () => {
     setErrors(newErrors);
 
     if (!newErrors.email && !newErrors.password) {
-      console.log(submitData);
+      setInvalidUser('');
+      setIsLoading(true);
+      submitData.type = 'login';
+      fetch(API_URL_ENDPOINT, {
+        redirect: 'follow',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'text/plain;charset=utf-8',
+        },
+        body: JSON.stringify(submitData),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.status === 'success') {
+            // Store user credentials in a cookie
+            setCookie('ifcaresSummer', JSON.stringify(data.data.id), 7);
+            router.push('/');
+            setIsLoading(false);
+          } else {
+            setIsLoading(false);
+            setInvalidUser(data.message);
+          }
+        })
+        .catch((e) => {
+          setIsLoading(false);
+          console.error(e);
+        });
     }
   };
 
@@ -54,7 +97,9 @@ const LoginForm = () => {
         height={200}
         priority
       />
-      <h2 className="text-xl font-medium mb-10 ml-3">Sign in to your account</h2>
+      <h2 className="text-xl font-medium mb-10 ml-3">
+        Sign in to your account
+      </h2>
       <div className="mb-10 relative">
         <motion.label
           htmlFor="email"
@@ -134,12 +179,21 @@ const LoginForm = () => {
           </span>
         )}
       </div>
+
       <button
         type="submit"
         className="text-white font-semibold rounded-lg text-base w-full px-5 py-2.5 text-center border bg-gradient-to-l from-green-500 to-blue-500"
       >
         Log In
       </button>
+      {isLoading && (
+        <div className='flex items-center justify-center mt-4'>
+          <Spinner />
+        </div>
+      )}
+      {invalidUser && (
+        <h6 className="text-xs text-red-600 text-center mt-4">{invalidUser}</h6>
+      )}
     </form>
   );
 };
